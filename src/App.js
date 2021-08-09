806fa34a0ff04bb792205b09c5ff97102284353c
Matheus Gomes <gomesprogramacao@gmail.com>
2021-08-09 14:00:47 -0300

    feat: scruture initial of dropdawn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React, { useState } from "react";
+
+import { Dropdown } from "./components/Dropdown";
 
 function App() {
+  const [selectedState, setSelectedState] = useState(null);
   return (
     <div className="App">
-       Hellio  
+
+    { selectedState && <div>O estado selecionado foi {selectedState}</div> }
+
+      <Dropdown
+        title="Selecione uma opção"
+        options={[
+          "Minas Gerais",
+          "São Paulo",
+          "Rio de Janeiro",
+          "Rio gradne do Sul",
+        ]}
+        onSelect={setSelectedState}
+      />
     </div>
   );
 }
---
+++ src/components/Dropdown/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
+
+export function Dropdown({ title, options, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelection = (option) => {
+    onSelect(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="dropdown">
+      <button onClick={() => setIsOpen(true)}>{title}</button>
+
+      {isOpen && (
+        <ul ole="menu">
+          {options.map((option) => (
+            <li
+              key={option}
+              role="menuitem"
+              onClick={() => handleSelection(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
